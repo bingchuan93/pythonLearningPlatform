5949e412ae4bf49d78e6d5ae42e17c94f9caadfc
Bing Chuan <bingchuan93@gmail.com>
2019-11-14 18:03:55 +0800

    Flask API

---
+++ flask/index.py
@@ -0,0 +1,42 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1993'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
+@app.route('/', methods=['GET'])
+def home():
+    print('Testing console')
+    return "<h1>BEE BAA BOO</h1>"
+
+@app.route('/books/all', methods=['GET'])
+def api_all():
+	code = """
+x = 2
+y = 3
+z = 3 * 2
+txt = input('Type:')
+print(z)
+	"""
+	exec(code)
+	return jsonify(books)
+
+app.run(port=7000)
