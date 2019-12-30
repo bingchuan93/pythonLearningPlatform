2e7dd93c0dda6e481c0a3b25974b2ec9c221263a
Bing Chuan <bingchuan93@gmail.com>
2019-12-30 15:23:33 +0800

    Complete tutorial group CRUD

--- plpAdmin/imports/ui/layouts/console/modal/tutorialGroups/base.js
+++ plpAdmin/imports/ui/layouts/console/modal/tutorialGroups/base.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import BaseModal from '/imports/ui/components/modal/base';
-import { FormGroup, Row, Col, Label } from 'reactstrap';
+import { FormGroup, Row, Col, Label, Button } from 'reactstrap';
 import { ValidatorForm } from 'react-form-validator-core';
 import TextValidator from '/imports/ui/components/validators/text';
 import Loader from '/imports/ui/components/icons/loader';
@@ -15,7 +15,8 @@ class TutorialGroupBase extends Component {
             form: {
                 name: '',
                 academicYear: new Date().getFullYear(),
-                semester: 0
+                semester: 0,
+                isArchived: false,
             }
         }
     }
@@ -25,22 +26,109 @@ class TutorialGroupBase extends Component {
             this.getTutorialGroup(this.props.id);
         }
     }
-    
+
     getTutorialGroup = (id) => {
         this.setState({ isFetching: true });
         Meteor.call('TutorialGroups.getById', id, (error, result) => {
             this.setState({ isFetching: false });
             if (!error) {
                 this.setState({
-                    form : {
+                    form: {
                         ...this.state.form,
                         name: result.name,
                         academicYear: result.academicYear,
-                        semester: result.semester
+                        semester: result.semester,
+                        isArchived: result.isArchived
                     }
                 });
             }
         })
+    }
+
+    handleArchive = (_id) => {
+        this.props.dispatch({
+            type: 'ALERT/OPEN', payload: {
+                alertProps: {
+                    body: (
+                        <div style={{ textAlign: "center" }}>
+                            Are you sure want to ARCHIVE this tutorial group?
+                        </div>
+                    ),
+                    closeOnBgClick: true,
+                    showConfirmButton: true,
+                    confirmButtonText: 'Yes',
+                    confirmButtonCallback: (e, closeAlert) => {
+                        e.preventDefault();
+                        Meteor.call('TutorialGroups.archive', _id, (error, result) => {
+                            closeAlert();
+                            if (error) {
+                                this.displayError(error.reason);
+                            }
+                            else {
+                                this.getTutorialGroup(_id.valueOf());
+                                this.props.dispatch({ type: 'CONTENT/FETCHABLE_TABLE_FORCE_FETCH' });
+                            }
+                        })
+                    },
+                    showCloseButton: true,
+                    closeButtonText: 'No',
+                }
+            }
+        });
+    }
+
+    handleRestore = (_id) => {
+        this.props.dispatch({
+            type: 'ALERT/OPEN', payload: {
+                alertProps: {
+                    body: (
+                        <div style={{ textAlign: "center" }}>
+                            Are you sure want to RESTORE this tutorial group?
+                        </div>
+                    ),
+                    closeOnBgClick: true,
+                    showConfirmButton: true,
+                    confirmButtonText: 'Yes',
+                    confirmButtonCallback: (e, closeAlert) => {
+                        e.preventDefault();
+                        Meteor.call('TutorialGroups.restore', _id, (error, result) => {
+                            closeAlert();
+                            if (error) {
+                                this.displayError(error.reason);
+                            }
+                            else {
+                                this.getTutorialGroup(_id.valueOf());
+                                this.props.dispatch({ type: 'CONTENT/FETCHABLE_TABLE_FORCE_FETCH' });
+                            }
+                        })
+                    },
+                    showCloseButton: true,
+                    closeButtonText: 'No',
+                }
+            }
+        });
+    }
+
+    displayError = (errorMsg) => {
+        this.props.dispatch({
+            type: 'ALERT/OPEN', payload: {
+                alertProps: {
+                    // icon: ErrorIcon,
+                    body: (
+                        <React.Fragment>
+                            <div className="alert-icon mb-2">
+                                {/* <ErrorIcon /> */}
+                            </div>
+                            <div style={{ textAlign: "center" }}>
+                                {errorMsg}
+                            </div>
+                        </React.Fragment>
+                    ),
+                    closeOnBgClick: true,
+                    showCloseButton: true,
+                }
+            }
+        });
     }
 
     render() {
@@ -145,7 +233,24 @@ class TutorialGroupBase extends Component {
                     </div>
                 }
                 footerClasses="justify-content-end"
-                footer={this.props.footer}
+                footer={
+                    <React.Fragment>
+                        {this.props.mode == 'read' && !this.state.isFetching &&
+                            <React.Fragment>
+                                {form.isArchived ? (
+                                    <Button color="success" size="sm" onClick={() => {
+                                        this.handleRestore(new Mongo.ObjectID(this.props.id))
+                                    }}>Restore</Button>
+                                ) : (
+                                    <Button color="secondary" size="sm" onClick={() => {
+                                        this.handleArchive(new Mongo.ObjectID(this.props.id))
+                                    }}>Archive</Button>
+                                )}
+                            </React.Fragment>
+                        }
+                        {this.props.footer}
+                    </React.Fragment>
+                }
             />
         );
     }
